1bf3533606014c0357fdedd02b2fbbd53c0e9cc1
Benjamin Linne <benjinne@gmail.com>
2018-04-08 14:00:46 -0400

    stuff

--- src/capture.py
+++ src/capture.py
@@ -1,8 +1,12 @@
 from cv2 import *
+from time import sleep
 import numpy as np
 
 # initialize the camera
-cam = VideoCapture(1)   # 0 -> index of camera
+cam = VideoCapture(0)   # 0 -> index of camera
+
+s, img = cam.read()
+sleep(.1)
 s, img = cam.read()
 if s:    # frame captured without any errors
 	namedWindow("cam-test",100)
